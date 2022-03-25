aece978c1d64f2fa8e3b536c3be555cb753920b6
MeghanaBusani <100418807+MeghanaBusani@users.noreply.github.com>
2022-03-25 20:05:09 +0530

    Add files via upload

---
+++ SimpleInterest.java
@@ -0,0 +1,20 @@
+//printing the Simple Interest
+import java.util.Scanner;
+public class SimpleInterest {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        //principal amount
+        System.out.println("Enter Principal amount:");
+        int P=sc.nextInt();
+        //time
+        System.out.println("Enter Time:");
+        int T=sc.nextInt();
+        //interest rate
+        System.out.println("Enter Interest rate:");
+        int R=sc.nextInt();
+        //Simple Interest
+        float SI=(P*T*R)/100;
+        System.out.println("Simple interest is:"+SI);
+
+    }
+}
