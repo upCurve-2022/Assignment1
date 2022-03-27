693304a0180ad57986cf55c5d91decef59fcaa8a
MeghanaBusani <100418807+MeghanaBusani@users.noreply.github.com>
2022-03-27 20:16:56 +0530

    Add files via upload

---
+++ EmptyString.java
@@ -0,0 +1,16 @@
+//to check whether a string is empty or not
+import java.util.Scanner;
+public class EmptyString {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter input String:");
+        String s=sc.nextLine();
+        int l=s.length();
+        if(l==0){
+            System.out.println("String is empty");
+        }
+        else{
+            System.out.println("String is not Empty");
+        }
+    }
+}
