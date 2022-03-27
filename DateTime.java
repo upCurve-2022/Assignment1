4f7c85e1866a0b72386b37bd43e079d853b8e7b9
MeghanaBusani <100418807+MeghanaBusani@users.noreply.github.com>
2022-03-27 23:04:31 +0530

    Add files via upload

---
+++ DateTime.java
@@ -0,0 +1,13 @@
+//printing current date and time
+import java.util.*;
+import java.time.*;
+public class DateTime {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        Date date_Time=new Date();
+        System.out.println("Current date and time:"+date_Time);
+        System.out.println("Current date and time:"+LocalDateTime.now());
+        System.out.println("Current date:"+LocalDate.now());
+        System.out.println("Current time:"+LocalTime.now());
+    }
+}
