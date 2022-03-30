b505b0b2911f1e32111b0925c4c829d7e085fff3
MeghanaBusani <100418807+MeghanaBusani@users.noreply.github.com>
2022-03-30 20:41:50 +0530

    Add files via upload

---
+++ NoOfWords.java
@@ -0,0 +1,17 @@
+//counting no of words in a string
+import java.util.Scanner;
+public class NoOfWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String");
+        String s = sc.nextLine();
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                count++;
+            }
+        }
+        System.out.println("No of Words:"+(count+1));
+    }
+
+}
