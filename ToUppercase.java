e95f51b22e3fde1e4c245be1163edaea8e9a4326
MeghanaBusani <100418807+MeghanaBusani@users.noreply.github.com>
2022-03-30 20:39:09 +0530

    Add files via upload

---
+++ ToUppercase.java
@@ -0,0 +1,13 @@
+//converting the string to uppercase
+import java.util.Scanner;
+public class ToUppercase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String");
+        String s = sc.nextLine();
+        System.out.println("uppercase:"+s.toUpperCase());
+    }
+
+
+}
+
