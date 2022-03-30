02f737e301582be4fa114c78315d20551adcbfd7
MeghanaBusani <100418807+MeghanaBusani@users.noreply.github.com>
2022-03-30 20:50:28 +0530

    Add files via upload

---
+++ Replacea.java
@@ -0,0 +1,16 @@
+//replacing a with $ in the given string
+import java.util.Scanner;
+public class Replacea {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter String");
+        String s=sc.nextLine();
+        StringBuilder str=new StringBuilder(s);
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='a'){
+                str.replace(i,i+1,"$");
+            }
+        }
+        System.out.println(str);
+    }
+}
